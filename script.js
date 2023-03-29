41cd18044911c16ef85d879259373e8a6da7e8f6
Ibrakhim_ <123295707+wewefewf@users.noreply.github.com>
2023-03-29 22:58:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// let day = +prompt('день');
+
+// if(day >= 1 && day < 11) {
+//     alert('1/3')
+// } else if (day >= 10 && day < 21) {
+//     alert('2/3')
+// } else if (day >= 20 && day <31) {
+//     alert('3/3')
+// } else {
+//     alert('ne morosi')
+// };
+
+
+
+
+// let name = prompt('Как вас зовут?').toLowerCase().trim();
+// let account =  prompt('Номер счета?');
+// let money = prompt('Сколько обналичить?');
+
+// if(name === 'alex' && account == 7777) {
+//     alert('Welcome')
+// } else {
+//     alert('Пользователь не найден, досвидули')
+// };
+
+// if(money >= 1 && money <= 10000) {
+//     alert('enter')
+// } else {
+//     alert('Недостаточно средств')
+// };
+
+
+let namee = prompt('Как тебя зовут?').toLowerCase().trim()
+let age = prompt('Сколько тебе лет?')
+let moneyy = prompt('Сколкьо у тебя денег?')
+
+if (namee[0] === 'a' && age >= 20 && age <= 40) {
+    alert('Welcome')
+} else {
+    alert('No')
+};
+
+let chel = prompt('Сколько вас?')
+if(moneyy >= 100 && chel <= 10) {
+    alert('Welcomee')
+} else {
+    alert('Noo')
+};
